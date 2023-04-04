697d14aafd73c75db54a53e6678459290c5195a0
Nikita Nemirovsky <vaze.legend@gmail.com>
2023-04-05 01:38:40 +0300

    Fix example

--- SocketStorm.Example/Program.cs
+++ SocketStorm.Example/Program.cs
@@ -2,19 +2,11 @@
 using SocketStorm.Server;
 
 using WebSocketServer server = new("*", 24517, "/ws/stream", WebSocketDataType.Text, 2, "my-protocol-v1");
-
-server.ConnectionOpened += (_, args) =>
-    Console.WriteLine($"Connection opened. Session id = {args.SessionId}, remote endpoint = {args.RemoteEndpoint}");
-
-server.ConnectionClosed += (_, args) => Console.WriteLine($"Connection closed. Session id = {args.SessionId}");
-
-server.MessageReceived += async (_, args) =>
-{
-    Console.WriteLine($"Message received from {args.SessionId}: {Encoding.UTF8.GetString(args.Data)}");
-    await server.SendAsync(Encoding.UTF8.GetBytes("echo: " + Encoding.UTF8.GetString(args.Data)), args.SessionId);
-};
-
-server.ExceptionThrown += (_, args) => throw args.Exception;
+CancellationTokenSource cts = new();
+server.ConnectionOpened += OnConnectionOpened;
+server.ConnectionClosed += OnConnectionClosed;
+server.MessageReceived += OnMessageReceived;
+server.ExceptionThrown += OnExceptionThrown;
 
 await server.StartAsync();
 
@@ -22,7 +14,31 @@ Console.CancelKeyPress += async (_, _) =>
 {
     await server.StopAsync();
     server.Dispose();
-    Environment.Exit(0);
+    cts.Cancel();
 };
 
-while (true) await Task.Delay(500);
+while (!cts.IsCancellationRequested) { }
+
+
+void OnConnectionOpened(object? _, ConnectionOpenedEventArgs args)
+{
+    Console.WriteLine($"Connection opened. Session id = {args.SessionId}, remote endpoint = {args.RemoteEndpoint}");
+}
+
+void OnConnectionClosed(object? _, ConnectionClosedEventArgs args)
+{
+    Console.WriteLine($"Connection closed. Session id = {args.SessionId}");
+}
+
+void OnMessageReceived(object? _, MessageReceivedEventArgs args)
+{
+    Console.WriteLine($"Message received from {args.SessionId}: {Encoding.UTF8.GetString(args.Data)}");
+    server.SendAsync(Encoding.UTF8.GetBytes("echo: " + Encoding.UTF8.GetString(args.Data)), args.SessionId)
+        .GetAwaiter()
+        .GetResult();
+}
+
+void OnExceptionThrown(object? _, ExceptionThrownEventArgs args)
+{
+    Console.WriteLine($"Exception thrown: {args.Exception}");
+}
