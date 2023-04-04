b702c6f1c4b0addb40bfeee96895f184546a5cf4
Nikita Nemirovsky <vaze.legend@gmail.com>
2023-04-04 22:11:23 +0300

    Add SocketStorm.Example project with simple example

---
+++ SocketStorm.Example/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using SocketStorm.Server;
+
+using WebSocketServer server = new("*", 24517, "/ws/stream", WebSocketDataType.Text, 2, "my-protocol-v1");
+
+server.ConnectionOpened += (_, args) =>
+    Console.WriteLine($"Connection opened. Session id = {args.SessionId}, remote endpoint = {args.RemoteEndpoint}");
+
+server.ConnectionClosed += (_, args) => Console.WriteLine($"Connection closed. Session id = {args.SessionId}");
+
+server.MessageReceived += async (_, args) =>
+{
+    Console.WriteLine($"Message received from {args.SessionId}: {Encoding.UTF8.GetString(args.Data)}");
+    await server.SendAsync(Encoding.UTF8.GetBytes("echo: " + Encoding.UTF8.GetString(args.Data)), args.SessionId);
+};
+
+server.ExceptionThrown += (_, args) => throw args.Exception;
+
+await server.StartAsync();
+
+Console.CancelKeyPress += async (_, _) =>
+{
+    await server.StopAsync();
+    server.Dispose();
+    Environment.Exit(0);
+};
+
+while (true) await Task.Delay(500);
